8872e12758683b237f435242b52b93518903342a
shadowalker <saeed.bayat@arusha.ir>
2021-09-05 22:42:49 +0430

    Implement getNextPrime function using js closure
    I did it this time :)))
    also implemented some test cases which are great in my opinion :)

--- system-group-interview/ceo-interview/is-prime.js
+++ system-group-interview/ceo-interview/is-prime.js
@@ -1,11 +1,22 @@
 const isPrime = (number) => {
     if (number === 1) return false;
     for (let i = 2; i <= number/2; i++) {
-        if (number % i === 0) return false
+        if (number % i === 0) return false;
     }
-    return true
+    return true;
 }
 
+const getNextPrime = (() => {
+    let current = 1;
+    return () => {
+        do {
+            current++;
+        } while (!isPrime(current));
+        return current
+    }
+})()
+
 module.exports = {
-    isPrime
+    isPrime,
+    getNextPrime
 }
--- system-group-interview/ceo-interview/is-prime.test.js
+++ system-group-interview/ceo-interview/is-prime.test.js
@@ -11,3 +11,24 @@ test("Should return if the number is prime or not", () => {
     expect(isPrime(257)).toBe(true);
     expect(isPrime(1023)).toBe(false);
 })
+
+const {getNextPrime} = require("./is-prime");
+
+test("Should return next prime number every time it's called", () => {
+    expect(getNextPrime()).toBe(2);
+    expect(getNextPrime()).toBe(3);
+    expect(getNextPrime()).toBe(5);
+    expect(getNextPrime()).toBe(7);
+    expect(getNextPrime()).toBe(11);
+    expect(getNextPrime()).toBe(13);
+    getNextPrime()
+    getNextPrime()
+    getNextPrime()
+    expect(getNextPrime()).toBe(29);
+    getNextPrime()
+    getNextPrime()
+    getNextPrime()
+    expect(getNextPrime()).toBe(43);
+    getNextPrime()
+    expect(getNextPrime()).toBe(53);
+})
