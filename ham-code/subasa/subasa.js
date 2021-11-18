df2745d058980f10bfb93124a2cf5d13a0312095
shadowalker <saeed.bayat@arusha.ir>
2021-11-18 22:36:42 +0330

    Add test cases and implement soloution

--- ham-code/subasa/subasa.js
+++ ham-code/subasa/subasa.js
@@ -1,9 +1,25 @@
 function isValidOrder(matchInfo, goalList) {
-    if (matchInfo.length != 3) return false;
-    const [goalCount, firstExtraTime, secondExtraTime] = matchInfo;
-    if (goalList.length != goalCount) return false;
-     
-    return "YES";
+  if (matchInfo.length != 3) return "NO";
+  const [goalCount, firstExtraTime, secondExtraTime] = matchInfo;
+  if (goalList.length != goalCount) return "NO";
+
+  const HALF_TIME = 45;
+  const FULL_TIME = 90;
+
+  let x = goalList[0];
+  for (let i = 1; i < goalList.length; i++) {
+    if (x <= goalList[i]) {
+      x = goalList[i];
+    } else {
+      if (x > HALF_TIME + firstExtraTime) {
+        return "NO";
+      }
+    }
+    if (goalList[i] > FULL_TIME + secondExtraTime) {
+      return "NO";
+    }
+  }
+  return "YES";
 }
 
 module.exports = isValidOrder;
--- ham-code/subasa/subasa.test.js
+++ ham-code/subasa/subasa.test.js
@@ -2,4 +2,10 @@ const isValidOrder = require("./subasa");
 
 test("Shoud retun YES if number order is valid", ()=> {
     expect(isValidOrder([4, 2, 4], [4, 45, 48, 93])).toBe("YES");
+    expect(isValidOrder([5, 3, 2], [4, 47, 45, 80, 91])).toBe("YES");
+});
+
+test("Shoud retun NO if number order is invalid", ()=> {
+    expect(isValidOrder([4, 2, 2], [4, 48, 45, 80])).toBe("NO");
+    expect(isValidOrder([6, 3, 2], [8, 48, 45, 70, 81, 94])).toBe("NO");
 });
