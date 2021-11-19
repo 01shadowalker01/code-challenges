bfe33dccd9adb7cc0fa8d3489325a90c032ed008
shadowalker <saeed.bayat@arusha.ir>
2021-11-19 17:36:51 +0330

    Problem soloution implemented

--- ham-code/Nov-2021/sierpinski-triangle/script.js
+++ ham-code/Nov-2021/sierpinski-triangle/script.js
@@ -1,1 +1,34 @@
-// TODO
+const container = document.querySelector("#container");
+const numberInput = document.querySelector("#number");
+const submitBtn = document.querySelector("#submit");
+
+submitBtn.addEventListener("click", () => {
+  generateTriangle(numberInput.value);
+});
+
+function generateTriangle(count) {
+  let rowList = [];
+  for (let i = 2; i <= Math.pow(2, count); i++) {
+    const row = createRow();
+    for (let j = 0; j < i; j++) {
+      const block = createBlock();
+      row.appendChild(block);
+    }
+    rowList.push(row);
+  }
+  rowList.forEach((row) => {
+    container.appendChild(row);
+  });
+}
+
+function createRow() {
+  const row = document.createElement("div");
+  row.classList = "row";
+  return row;
+}
+
+function createBlock() {
+  const div = document.createElement("div");
+  div.classList = "block active";
+  return div;
+}
